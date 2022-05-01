f1bb2c4404ae273092441c0d86f7ec3d8e7a036a
Jose Lemuz <lemuz.jose@gmail.com>
2022-05-01 16:04:03 -0700

    can create manager object from inquirer

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const inquirer = require('inquirer');
-const generatePage = require('./src/page-template');
-const { writeFile, copyFile } = require('./utils/generate-site');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+
+//const generatePage = require('./src/page-template');
+//const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -19,10 +24,10 @@ const promptUser = () => {
     },
     {
       type: 'input',
-      name: 'ID',
+      name: 'id',
       message: 'Enter your employee ID (Required)',
-      validate: githubInput => {
-        if (githubInput) {
+      validate: idInput => {
+        if (idInput) {
           return true;
         } else {
           console.log('Please enter your employee ID!');
@@ -34,8 +39,8 @@ const promptUser = () => {
         type: 'input',
         name: 'email',
         message: 'Enter your email address (Required)',
-        validate: githubInput => {
-          if (githubInput) {
+        validate: emailInput => {
+          if (emailInput) {
             return true;
           } else {
             console.log('Please enter your email address!');
@@ -47,8 +52,8 @@ const promptUser = () => {
         type: 'input',
         name: 'office',
         message: 'Enter your office number (Required)',
-        validate: githubInput => {
-          if (githubInput) {
+        validate: officeInput => {
+          if (officeInput) {
             return true;
           } else {
             console.log('Please enter your office number!');
@@ -56,105 +61,10 @@ const promptUser = () => {
           }
         }
       }
-  ]);
+  ]).then(answers=>{
+      let manager = new Manager(answers.name, answers.id, answers.email, answers.office);
+      console.log(manager)
+  })
 };
 
-const promptProject = portfolioData => {
-  console.log(`
-=================
-Add a New Project
-=================
-`);
-
-  // If there's no 'projects' array property, create one
-  if (!portfolioData.projects) {
-    portfolioData.projects = [];
-  }
-  return inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of your project? (Required)',
-        validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log('You need to enter a project name!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a description of the project (Required)',
-        validate: descriptionInput => {
-          if (descriptionInput) {
-            return true;
-          } else {
-            console.log('You need to enter a project description!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'checkbox',
-        name: 'languages',
-        message: 'What did you this project with? (Check all that apply)',
-        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-      },
-      {
-        type: 'input',
-        name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)',
-        validate: linkInput => {
-          if (linkInput) {
-            return true;
-          } else {
-            console.log('You need to enter a project GitHub link!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'confirm',
-        name: 'feature',
-        message: 'Would you like to feature this project?',
-        default: false
-      },
-      {
-        type: 'confirm',
-        name: 'confirmAddProject',
-        message: 'Would you like to enter another project?',
-        default: false
-      }
-    ])
-    .then(projectData => {
-      portfolioData.projects.push(projectData);
-      if (projectData.confirmAddProject) {
-        return promptProject(portfolioData);
-      } else {
-        return portfolioData;
-      }
-    });
-};
-
-promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    return generatePage(portfolioData);
-  })
-  .then(pageHTML => {
-    return writeFile(pageHTML);
-  })
-  .then(writeFileResponse => {
-    console.log(writeFileResponse);
-    return copyFile();
-  })
-  .then(copyFileResponse => {
-    console.log(copyFileResponse);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+promptUser();
