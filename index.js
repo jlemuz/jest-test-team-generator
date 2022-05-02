c07cd8cb2c5e6bdd5bd5d6af9e5f1a3691dc1229
Jose Lemuz <lemuz.jose@gmail.com>
2022-05-02 13:47:18 -0700

    added intern/engineer objects through inquirer

--- index.js
+++ index.js
@@ -63,8 +63,156 @@ const promptUser = () => {
       }
   ]).then(answers=>{
       let manager = new Manager(answers.name, answers.id, answers.email, answers.office);
-      console.log(manager)
+      console.log(manager);
+      addMembers();
   })
 };
 
+//Menu to allow user to choose what team member to add
+function addMembers(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'memberType',
+            message: 'Would you like to add another member?',
+            choices: [
+                "Engineer",
+                "Intern",
+                "No more members"]
+        }
+    ]).then(option =>{
+        //Depending on selection user will be directed to appropriate prompts
+        if(option.memberType == 'Engineer'){
+            addEngineer();
+        }
+        else if (option.memberType == 'Intern'){
+            addIntern();
+        }
+        else( console.log('No more members'))
+    })
+};
+
+
+const addEngineer = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the engineer\'s name? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your name!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Enter your employee\'s ID (Required)',
+        validate: idInput => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log('Please enter your employee ID!');
+            return false;
+          }
+        }
+      },
+      {
+          type: 'input',
+          name: 'email',
+          message: 'Enter engineer\'s email address (Required)',
+          validate: emailInput => {
+            if (emailInput) {
+              return true;
+            } else {
+              console.log('Please enter your email address!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'github',
+          message: 'Enter github username (Required)',
+          validate: officeInput => {
+            if (officeInput) {
+              return true;
+            } else {
+              console.log('Please enter github username!');
+              return false;
+            }
+          }
+        }
+    ]).then(answers=>{
+        let engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        console.log(engineer);
+        addMembers();
+    })
+  };
+
+  
+const addIntern = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the intern\'s name? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your intern\'s name!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Enter your intern\'s ID (Required)',
+        validate: idInput => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log('Please enter your intern\'s employee ID!');
+            return false;
+          }
+        }
+      },
+      {
+          type: 'input',
+          name: 'email',
+          message: 'Enter intern\'s email address (Required)',
+          validate: emailInput => {
+            if (emailInput) {
+              return true;
+            } else {
+              console.log('Please enter your intern\'s email address!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'school',
+          message: 'Enter github username (Required)',
+          validate: schoolInput => {
+            if (schoolInput) {
+              return true;
+            } else {
+              console.log('Please enter school!');
+              return false;
+            }
+          }
+        }
+    ]).then(answers=>{
+        let intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        console.log(intern);
+        addMembers();
+    })
+  };
 promptUser();
